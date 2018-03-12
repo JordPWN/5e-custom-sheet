88e77285d17a5972d8401feeb1fed2d01f4368a9
Jordan Ratanadawong <jordan.ratanadawong@gmail.com>
2018-03-12 14:58:30 -0400

    Expanding libraries.

---
+++ src/libraries/character-info/subclasses.js
@@ -0,0 +1,67 @@
+const subclasses = {
+  'barbarian': [
+    'ancestral guardian',
+    'battlerager',
+    'berserker',
+    'storm herald',
+    'totem warrior',
+    'zealot'
+ ],
+  'bard': [
+    'glamour',
+    'lore',
+    'swords',
+    'valor',
+    'whispers'
+  ],
+  'cleric': [
+    'arcana',
+    'death',
+    'forge',
+    'grave',
+    'knowledge',
+    'life',
+    'light',
+    'nature',
+    'tempest',
+    'trickery',
+    'war'
+  ],
+  'druid': [
+    'dreams',
+    'land',
+    'moon',
+    'shepherd'
+  ],
+  'fighter': [
+    'arcane archer',
+    'purple dragon',
+    'battlemaster',
+    'cavalier',
+    'champion',
+    'eldritch knight',
+    'samurai'
+  ],
+  'monk': [
+    'drunken master',
+    'four elements',
+    'kensai',
+    'long death',
+    'open hand',
+    'shadow',
+    'sun soul'
+  ],
+  'paladin': [
+    'ancients',
+    'conquest',
+    'crown',
+    'devotion',
+    'redemption',
+    'vengeance',
+    'oathbreaker'
+  ],
+  'ranger',
+  'sorcerer',
+  'warlock',
+  'wizard'
+}
---
+++ src/libraries/weapons.js
@@ -0,0 +1,88 @@
+const weapons = {
+  'club': {
+    cost: '1sp',
+    damage: [1, 4],
+    damageType: 'bludgeoning',
+    properties: ['light'],
+    range: 'melee',
+    weight: 2
+  },
+  'dagger': {
+    cost: '2gp',
+    damage: [1, 4],
+    damageType: 'piercing',
+    properties: ['finesse', 'light', 'thrown'],
+    range: 'melee',
+    thrownRange: [20, 60],
+    weight: 1
+  },
+  'greatclub': {
+    cost: '2sp',
+    damage: [1, 8],
+    damageType: 'bludgeoning',
+    properties: ['two-handed'],
+    range: 'melee',
+    weight: 10
+  },
+  'handaxe': {
+    cost: '5gp',
+    damage: [1, 6],
+    damageType: 'slashing',
+    properties: ['light', 'thrown'],
+    range: 'melee',
+    thrownRange: [20, 60],
+    weight: 2
+  },
+  'javelin': {
+    cost: '5sp',
+    damage: [1, 5],
+    damageType: 'piercing',
+    properties: ['thrown'],
+    range: 'melee',
+    thrownRange: [30, 120],
+    weight: 2
+  },
+  'light hammer': {
+    cost: '2gp',
+    damage: [1, 4],
+    damageType: 'bludgeoning',
+    properties: ['light', 'thrown'],
+    range: 'melee',
+    thrownRange: [20, 60],
+    weight: 2
+  },
+  'mace': {
+    cost: '5gp',
+    damage: [1, 6],
+    damageType: 'bludgeoning',
+    range: 'melee',
+    weight: 4
+  },
+  'quarterstaff': {
+    cost: '2sp',
+    damage: [1, 6],
+    damageType: 'bludgeoning',
+    properties: ['versatile (1d8)'],
+    range: 'melee',
+    weight: 4
+  },
+  'sickle': {
+    cost: '1gp',
+    damage: [1, 4],
+    damageType: 'slashing',
+    properties: ['light'],
+    range: 'melee',
+    weight: 2
+  },
+  'spear': {
+    cost: '1gp',
+    damage: [1, 6],
+    damageType: 'piercing',
+    properties: ['thrown', 'versatile (1d8)'],
+    range: 'melee',
+    thrownRange: [20, 60],
+    weight: 3
+  },
+}
+
+export default weapons
